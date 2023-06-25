5dbb32876f801fe8216e77dc0fe713b379383094
Rojack96 <rojack96@protonmail.com>
2023-06-25 15:24:51 +0200

    naming fix and create codec8string

--- codecs/codec_8/string.go
+++ codecs/codec_8/string.go
@@ -1,1 +1,17 @@
 package codec8
+
+import (
+	"encoding/hex"
+
+	models "github.com/rojack96/teltonika-parser/models/codec_8"
+)
+
+func Codec8String(dataPacket string) models.AVLDataArray {
+	dataPacketBytes, err := hex.DecodeString(dataPacket)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return Codec8Bytes(dataPacketBytes)
+}
