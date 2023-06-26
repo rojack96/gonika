2b76d8337297a2cbef87ace74e3de0b437fa0a71
Rojack96 <rojack96@protonmail.com>
2023-06-26 22:58:28 +0200

    init codec 16

---
+++ codecs/codec_16/bytes.go
@@ -0,0 +1,66 @@
+package codec16
+
+import (
+	"encoding/binary"
+
+	"github.com/rojack96/teltonika-parser/helpers"
+	models "github.com/rojack96/teltonika-parser/models/codec_16"
+)
+
+func Codec16Bytes(dataPacket []byte) models.AVLDataArray {
+	var avlDataArray models.AVLDataArray
+
+	avlDataPacket := helpers.DataBytesParser(&dataPacket)
+
+	avlDataArray.Preamble = binary.BigEndian.Uint32(avlDataPacket.Preamble)
+	avlDataArray.CodecID = avlDataPacket.CodecID
+	avlDataArray.NumberOfData1 = avlDataPacket.NumberOfData1
+	avlDataArray.NumberOfData2 = avlDataPacket.NumberOfData2
+	avlDataArray.CRC16 = binary.BigEndian.Uint32(avlDataPacket.CRC16)
+
+	body := avlDataPacket.AVLdata
+
+	startIndex := 0
+
+	if avlDataArray.NumberOfData1 == avlDataArray.NumberOfData2 {
+		for i := 0; i < int(avlDataArray.NumberOfData1); i++ {
+			avlData := models.AVLData{}
+
+			var timestampEndIndex int
+			avlData.Timestamp, timestampEndIndex = parseTimestamp(startIndex, body)
+
+			var priorityIndex int
+			avlData.Priority, priorityIndex = parsePriority(timestampEndIndex, body)
+
+			var gpsEndIndex int
+			avlData.GPSElement, gpsEndIndex = parseGPSElement(priorityIndex, body)
+
+			var eventIOIDIndexEnd int
+			avlData.EventIOID, eventIOIDIndexEnd = parseEventIO(gpsEndIndex, body)
+
+			var generationTypeIndexEnd int
+			avlData.GenerationType, generationTypeIndexEnd = parseGenerationType(eventIOIDIndexEnd, body)
+
+			var noOfTotalIOIndexEnd int
+			avlData.NoOfTotalIO, noOfTotalIOIndexEnd = parseTotalNumberOfIO(generationTypeIndexEnd, body)
+
+			var oneByteIOEndIndex int
+			avlData.NoOfOneByte, avlData.OneByteIO, oneByteIOEndIndex = parseOneByteIO(noOfTotalIOIndexEnd, body)
+
+			var twoByteIOEndIndex int
+			avlData.NoOfTwoByte, avlData.TwoByteIO, twoByteIOEndIndex = parseTwoByteIO(oneByteIOEndIndex, body)
+
+			var fourByteIOEndIndex int
+			avlData.NoOfFourByte, avlData.FourByteIO, fourByteIOEndIndex = parseFourByteIO(twoByteIOEndIndex, body)
+
+			var eightByteIOEndIndex int
+			avlData.NoOfEightByte, avlData.EightByteIO, eightByteIOEndIndex = parseEightByteIO(fourByteIOEndIndex, body)
+
+			startIndex = eightByteIOEndIndex
+
+			avlDataArray.AVLData = append(avlDataArray.AVLData, avlData)
+		}
+	}
+
+	return avlDataArray
+}
---
+++ models/codec_16/bytes.go
@@ -0,0 +1,34 @@
+package codec16
+
+import "github.com/rojack96/teltonika-parser/models"
+
+type AVLDataArray struct {
+	models.AVLDataArray
+	AVLData []AVLData `json:"avl_data"`
+}
+
+type AVLData struct {
+	models.AVLDataTsToGps
+	// Event IO ID (1 byte) this field defines which IO property has changed and generated an event.
+	EventIOID uint16 `json:"event_IO_id"`
+	// Data event generation type
+	GenerationType uint8 `json:"generation_type"`
+	// Number of Total IO (1 byte) a total number of properties coming with record (N = N1 + N2 + N4 + N8).
+	NoOfTotalIO uint8 `json:"n_of_tolat_IO"`
+	// Number of One Byte IO (1 byte) number of properties which length is 1 byte.
+	NoOfOneByte uint8 `json:"-"`
+	// Number of Two Byte IO (1 byte) number of properties which length is 2 bytes.
+	NoOfTwoByte uint8 `json:"-"`
+	// Number of Four Byte IO (1 byte) number of properties which length is 4 bytes.
+	NoOfFourByte uint8 `json:"-"`
+	// Number of Eight Byte IO (1 byte) number of properties which length is 8 bytes.
+	NoOfEightByte uint8 `json:"-"`
+	// Map id:value with properties which length is 1 byte.
+	OneByteIO map[uint16]uint8 `json:"-"`
+	// Map id:value with properties which length is 2 bytes.
+	TwoByteIO map[uint16]uint16 `json:"-"`
+	// Map id:value with properties which length is 4 bytes.
+	FourByteIO map[uint16]uint32 `json:"-"`
+	// Map id:value with properties which length is 8 bytes.
+	EightByteIO map[uint16]uint64 `json:"-"`
+}
